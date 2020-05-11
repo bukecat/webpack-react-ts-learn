a0874aaed4644aa6e26243fa969b3a0969a7a238
bukecat <342231678@qq.com>
2020-05-11 18:34:56 +0800

    -

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
-modules.exports = {
-  entry: './src/index.tsx',
+module.exports = {
+  entry: "./src/index.tsx",
   output: {
-    filename: 'bundle.js',
-    path: __dirname + '/dist'
+    filename: "bundle.js",
+    path: __dirname + "/dist"
   },
 
-  devtool: 'source-map',
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
 
   resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: [".ts", ".tsx", ".js", ".json"]
   },
 
@@ -21,6 +23,10 @@ modules.exports = {
     ]
   },
 
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
   externals: {
     "react": "React",
     "react-dom": "ReactDOM"
