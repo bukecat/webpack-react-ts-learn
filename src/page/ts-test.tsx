bef59fd1a074310f8883b158a91eb79845d0800c
bukecat <342231678@qq.com>
2020-05-15 18:11:15 +0800

    -

--- src/page/ts-test.tsx
+++ src/page/ts-test.tsx
@@ -1,5 +1,33 @@
 import * as React from 'react';
 
+
+
+/* 数组 */
+let array_list1: Array<any> = [1, '2', 3]; // right
+// let array_list2: Array<number> = [1, '2', 3]; // error，必须全部number类型
+
+let array_list3: any[] = [1, '2', 3]; // right
+// let array_list4: Number[] = [1, '2', 3]; // error，必须全部number类型
+
+
+
+/* 元组 */
+let tuple1: [number, string];
+tuple1 = [1, '1']; // right
+// tuple1 = ['1', '1']; // error, 必须对应类型
+
+// tuple1[1].substr(1); // right
+// tuple1[0].substr(1); // error, number没有substr方法
+
+tuple1[2] = '1';
+
+// tuple1[3] = true;
+
+console.log(tuple1);
+
+
+
+/* 枚举 */
 enum Color {
   Red, Green, Blue = 4
 }
